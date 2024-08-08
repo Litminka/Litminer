7442bf405fcf20d6041a0f8104d2ab277a8231e1
ArcKontyR <kontyrtime@mail.ru>
2024-08-08 23:45:51 +0500

    env example changes

--- environment.d.ts
+++ environment.d.ts
@@ -2,9 +2,13 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NODE_ENV: 'dev' | 'prod';
+            DISCORD_BOT_LOGIN: string;
+            DISCORD_BOT_PASSWORD: string;
             DISCORD_BOT_TOKEN: string;
             DISCORD_BOT_ID: string;
             GUILD_ID: string;
+            LITMINKA_TOKEN: string;
+            LITMINKA_REFRESH_TOKEN: string;
             DATABASE_URL: string;
             REDIS_HOST: string;
             REDIS_PORT: string;
