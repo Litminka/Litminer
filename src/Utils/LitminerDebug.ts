92110047154b3776c9bb535e97eea3b56c8390d6
ArcKontyR <kontyrtime@mail.ru>
2024-08-15 17:53:15 +0500

    potentially fixed debugger

--- src/Utils/LitminerDebug.ts
+++ src/Utils/LitminerDebug.ts
@@ -1,36 +1,36 @@
 export class LitminerDebug {
-    static Log(message: string, color: DebugColor) {
+    static Log(message: any, color: DebugColor) {
         if (!process.env.DEBUG) return;
 
         console.log(`[LitminerV2] \u001b[${color}m${message}\u001b[0m`)
     }
 
-    public static Error(message: string) {
+    public static Error(message: any) {
         LitminerDebug.Log(`[ERROR] ${message}`, DebugColor.Red);
     }
 
-    public static Info(message: string) {
+    public static Info(message: any) {
         LitminerDebug.Log(`[INFO] ${message}`, DebugColor.Cyan);
     }
 
-    public static Warning(message: string) {
+    public static Warning(message: any) {
         LitminerDebug.Log(`[WARNING] ${message}`, DebugColor.Yellow);
     }
 
-    public static Special(message: string) {
+    public static Special(message: any) {
         LitminerDebug.Log(`[SPECIAL] ${message}`, DebugColor.Purple);
     }
 
-    public static Debug(message: string) {
+    public static Debug(message: any) {
         LitminerDebug.Log(`[DEBUG] ${message}`, DebugColor.White);
     }
 
-    public static Success(message: string) {
+    public static Success(message: any) {
         LitminerDebug.Log(`[SUCCESS] ${message}`, DebugColor.Green);
     }
 }
 
-export enum DebugColor {
+enum DebugColor {
     White = 0,
     Red = 31,
     Green = 32,
